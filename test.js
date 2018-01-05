267648d8a486f6b4bb5ae99f5abe42d303571f38
silverwind <me@silverwind.io>
2018-01-06 00:02:11 +0100

    allow ppid 0 in tests for travis

--- test.js
+++ test.js
@@ -4,12 +4,12 @@ const fs = require("fs");
 const fork = require("child_process").fork;
 const assert = require("assert");
 
-// This tests whether the parent pid of the daemonized process is 1 which is
-// true on Linux and macOS at least.
+// This tests whether the parent pid of the daemonized process is 0 or 1 which
+// indicates the process was correctly orphaned.
 
 fork("test-child.js").on("exit", function() {
   setTimeout(function() {
-    assert.equal(fs.readFileSync("ppid", "utf8"), "1");
+    assert(["0", "1"].includes(fs.readFileSync("ppid", "utf8")));
     fs.unlinkSync("ppid");
   }, 100);
 });
