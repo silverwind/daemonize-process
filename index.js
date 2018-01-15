be6ffc9b599daec3aeb30051a70ade408b06f112
silverwind <me@silverwind.io>
2018-01-15 09:14:12 +0100

    save a few processor cycles in the child by inlining opts

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 "use strict";
 
 const id = "_DAEMONIZE_PROCESS";
-const defaults = {
-  // spawn options
-  env: process.env,
-  cwd: process.cwd(),
-  stdio: "ignore",
-  detached: true,
-  // custom options
-  node: process.execPath,
-  script: process.argv[1],
-  arguments: process.argv.slice(2),
-  exitCode: 0,
-};
 
 module.exports = function(opts) {
   if (id in process.env) {
@@ -20,7 +8,18 @@ module.exports = function(opts) {
     delete process.env[id];
   } else {
     // In the parent, set the tracking environment variable, fork the child and exit
-    opts = Object.assign({}, defaults, opts);
+    opts = Object.assign({}, {
+      // spawn options
+      env: process.env,
+      cwd: process.cwd(),
+      stdio: "ignore",
+      detached: true,
+      // custom options
+      node: process.execPath,
+      script: process.argv[1],
+      arguments: process.argv.slice(2),
+      exitCode: 0,
+    }, opts);
     opts.env = Object.assign({}, opts.env, {[id]: "1"});
     require("child_process").spawn(opts.node, [opts.script].concat(opts.arguments), opts).unref();
     process.exit(opts.exitCode);
